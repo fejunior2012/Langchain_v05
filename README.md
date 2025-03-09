94828abec2c62d2f6306c873fcc8c1676ecd0b47
fejunior2012 <45799110+fejunior2012@users.noreply.github.com>
2025-03-09 18:13:10 -0300

    Primeira scripts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Langchain_v05
- https://www.youtube.com/watch?v=q9wrnE_32Rs
+# Como usar o GPT com seus próprios dados?
+Código gerado através do canal Asimov Academy (https://www.youtube.com/@AsimovAcademy) e vídeo Como usar o GPT com seus próprios dados? (https://www.youtube.com/watch?v=rOjusRRO1EI).
+
+Jesus Cristo é o Senhor
---
+++ email_qa.py
@@ -0,0 +1,18 @@
+# pip install streamlit
+# pip install langchain
+# pip install langchain_community
+# pip install python-dotenv
+import streamlit as st
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate # exemplo prompt
+from langchain.chat_models import ChatOpenAI # modelo utilizado
+from langchain.chains import LLMChain # conecta tudo
+from dotenv import load_dotenv
+from langchain_community.document_loaders import CSVLoader
+
+# Carrega a chave Open AI
+load_dotenv()
+
+loader = CSVLoader(file_path="knowledge_base.csv")
+documents = loader.load()
