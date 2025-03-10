b3280513dd46053e26ee3f2d860551bdfb56aaae
fejunior2012 <45799110+fejunior2012@users.noreply.github.com>
2025-03-09 21:24:25 -0300

    Funcionando

--- email_qa.py
+++ email_qa.py
@@ -2,16 +2,86 @@
 # pip install -U langchain langchain-community
 # pip install python-dotenv
 import streamlit as st
-from langchain.vectorstores import FAISS
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate # exemplo prompt
-from langchain.chat_models import ChatOpenAI # modelo utilizado
 from langchain.chains import LLMChain # conecta tudo
 from dotenv import load_dotenv
 from langchain_community.document_loaders import CSVLoader
+from langchain_community.document_loaders import UnstructuredEmailLoader, TextLoader
 
-# Carrega a chave Open AI
+# Carrega a chave Open AI 
 load_dotenv()
 
 loader = CSVLoader(file_path="knowledge_base.csv")
 documents = loader.load()
+
+# Carregar mais e-maisl
+# email_loader = UnstructuredEmailLoader("emails_arquivo.eml")
+text_loader = TextLoader("EMAIL00001.txt")
+documents += text_loader.load()
+text_loader = TextLoader("EMAIL00002.txt")
+documents += text_loader.load()
+
+
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(documents, embeddings)
+
+def retrieve_info(query):
+    similar_response = db.similarity_search(query, k=3)
+    return [doc.page_content for doc in similar_response] if similar_response else []
+
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+
+template = """
+    Você um assistente virtual de uma escola de programação focada na linguaguem python.
+    Sua função será responder e-mails que recebemos de potenciais clientes.
+    Vou lhe passar alguns e-mails antigos enviados por nosso time de vendas para que você use como modelo
+
+    Siga todas as regras abaixo:
+    1/ Você deve busca se comportar de maneira semelhante à roberta@email.com
+
+    2/ Suas respostas devem ser bem similares ou até identicas às enviadas por ela no passado, tanto em termos de comprimento, tom de voz, argumentos lógicos e demais detalhes.
+
+    3/ Alguns dos e-mails podem conter links e informações irrelevantes. Preste atenção apenas ao contéudo útil da mensagem.
+
+    Arqui está uma mensagem recebida de um novo cliente.
+    {message}
+
+    Aqui está uma lista de e-mails trocados anteriormente antre outros clientes e nossa atendente.
+    Este histórico de conversa servirá de base para que voce compreenda nossos produto e forma de atendimento
+    {best_practice}
+
+    Escreva a melhor resposta que eu deveria enviar para este potencial cliente:
+"""
+
+prompt = PromptTemplate(
+    input_variables=["message", "best_practice"],
+    template=template
+)
+
+chain = LLMChain(llm=llm, prompt=prompt)
+def generate_response(message):
+    best_practice = retrieve_info(message)
+    response = chain.invoke({"message": message, "best_practice": best_practice})
+    return response["text"] if "text" in response else response
+
+
+if db:
+    print("Base FAISS carregada com sucesso!")
+else:
+    print("Erro ao carregar a base FAISS.")
+
+if llm:
+    print("Modelo de IA carregado corretamente!")
+else:
+    print("Erro ao carregar o modelo de IA.")
+
+# response = generate_response("Olá, eu posso parcelar o curso?")
+# print(response)
+
+response = generate_response("""
+                               Boa tarde, tudo bem? As aulas são gravadas ou ao vivo? Posso assistir até qual prazo máximo?
+                             """)
+print(response)
